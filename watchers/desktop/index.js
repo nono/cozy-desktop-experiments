f25dfca943c61206a60fe80a5ac0cb5c2987d8a0
Bruno Michel <bmichel@menfin.info>
2018-09-26 16:55:59 +0200

    Really add files/folders to pouchdb

--- watchers/desktop/index.js
+++ watchers/desktop/index.js
@@ -1,54 +1,60 @@
 #!/usr/bin/env node
 
+const Ignore = require('desktop/core/ignore')
+const Merge = require('desktop/core/merge')
 const Pouch = require('desktop/core/pouch')
+const Prep = require('desktop/core/prep')
 const Watcher = require('desktop/core/local/watcher')
 
 const syncPath = '../workspace' // TODO
-
 const config = {
   dbPath: '../tmp' // TODO
 }
+const pouch = new Pouch(config)
 
-const prep = {
+const ignore = new Ignore('')
+const merge = new Merge(pouch)
+const prep = new Prep(merge, ignore, config)
+
+const loggerPrep = {
   addFileAsync: (side, doc) => {
-    console.log('F +', doc)
-    return Promise.resolve()
+    console.log('File +', doc)
+    return prep.addFileAsync(side, doc)
   },
   moveFileAsync: (side, doc, old) => {
-    console.log('F <', old, '>', doc)
-    return Promise.resolve()
+    console.log('File <', old, '>', doc)
+    return prep.moveFileAsync(side, doc, old)
   },
   moveFolderAsync: (side, doc, old) => {
-    console.log('D <', old, '>', doc)
-    return Promise.resolve()
+    console.log('Dir  <', old, '>', doc)
+    return prep.moveFolderAsync(side, doc, old)
   },
   putFolderAsync: (side, doc) => {
-    console.log('D +', doc)
-    return Promise.resolve()
+    console.log('Dir  +', doc)
+    return prep.putFolderAsync(side, doc)
   },
   trashFileAsync: (side, doc) => {
-    console.log('F -', doc)
-    return Promise.resolve()
+    console.log('File -', doc)
+    return prep.trashFileAsync(side, doc)
   },
   trashFolderAsync: (side, doc) => {
-    console.log('D -', doc)
-    return Promise.resolve()
+    console.log('Dir  -', doc)
+    return prep.trashFolderAsync(side, doc)
   },
   updateFileAsync: (side, doc) => {
-    console.log('F *', doc)
-    return Promise.resolve()
+    console.log('File *', doc)
+    return prep.updateFileAsync(side, doc)
   },
 }
 
 const events = {
   emit: (msg) => {
-    console.log('E', msg)
+    console.log('Info', msg)
   }
 }
 
-const pouch = new Pouch(config)
 pouch.addAllViews(() => {
-  const w = new Watcher(syncPath, prep, pouch, events)
+  const w = new Watcher(syncPath, loggerPrep, pouch, events)
   w.start()
   const quit = () => {
     w.stop()
