e773db7d2b2fbe89cfa6dd56def42baa7f2b3a4b
Bruno Michel <bmichel@menfin.info>
2019-12-30 18:40:29 +0100

    Start playing with inotify

--- spec/desktop_spec.cr
+++ spec/desktop_spec.cr
@@ -1,9 +1,7 @@
 require "./spec_helper"
 
 describe Desktop do
-  # TODO: Write tests
-
   it "works" do
-    false.should eq(true)
+    Desktop.start "."
   end
 end
--- src/desktop.cr
+++ src/desktop.cr
@@ -1,6 +1,16 @@
+require "inotify"
+
 # TODO: Write documentation for `Desktop`
 module Desktop
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  def self.start(dir)
+    watcher = Inotify.watch dir do |event|
+      pp! event
+    end
+
+    # ... for 10 seconds.
+    sleep 10.seconds
+    watcher.close
+  end
 end
