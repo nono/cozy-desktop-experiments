40c2dc3964bfd05519bf9acd7e42519d5bccef2a
Bruno Michel <bmichel@menfin.info>
2020-02-09 17:16:12 +0100

    Add the OS for clients to be future-proof

--- src/simulator/scenario.cr
+++ src/simulator/scenario.cr
@@ -2,20 +2,23 @@ require "./operation"
 
 module Simulator
   struct Scenario
+    alias Client = NamedTuple(name: String, OS: String)
+
     property pending : String?
-    property clients : Array(String)
+    property clients : Array(Client)
     property ops : Array(Operation)
 
     def initialize(nb_clients)
       case nb_clients
       when 1
-        @clients = ["desktop"]
+        @clients = [{name: "desktop", os: "linux"}]
       when 2
-        @clients = ["desktop", "laptop"]
+        @clients = [{name: "desktop", os: "linux"},
+                    {name: "laptop", os: "linux"}]
       else
         @clients = [] of String
         nb_clients.times do
-          @clients << Faker::Internet.user_name
+          @clients << {name: Faker::Internet.user_name, os: "linux"}
         end
       end
       @ops = [] of Operation
