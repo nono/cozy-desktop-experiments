9d9c5da336a1f6dcefcb22d9a8f362b35f508ba9
Bruno Michel <bmichel@menfin.info>
2020-03-21 22:24:10 +0100

    Use prettier 2.0

--- src/web/public/dev.js
+++ src/web/public/dev.js
@@ -1,10 +1,10 @@
-;(function(window, document) {
+;(function (window, document) {
   const $ = document.querySelector.bind(document)
   const $$ = document.querySelectorAll.bind(document)
 
   // Setting is-active on the active link should be done in the view, but kemal
   // doesn't offer an easy way to do that, so JS, it is.
-  $$(".sidebar a").forEach(link => {
+  $$(".sidebar a").forEach((link) => {
     if (link.href === window.location.href) {
       link.classList.add("is-active")
     }
